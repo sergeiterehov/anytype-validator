f929f1879ed018ad6fd4a6daccb1e1a7168d607f
Sergei Terehov <isergeiterehov@gmail.com>
2019-06-30 22:56:56 +0300

    Добавлена валидация

--- defaultTypes.js
+++ defaultTypes.js
@@ -4,6 +4,8 @@ const { Uses } = require("./index");
 const validate = (types, typeName, uses, path, target, props) => {
     const type = types[typeName];
 
+    // console.log(path, type, target, props)
+
     if (! type) {
         return `Type "${typeName}" for ${path} not found`;
     }
@@ -48,7 +50,7 @@ const validateObject = (types, type, uses, path, target, props) => {
     }
 };
 
-const validateArray = (types, type, uses, path, target, props) => {
+const validateArrayOf = (types, type, uses, path, target, props) => {
     if (target === undefined || target === null) {
         if (uses === Uses.Required) {
             return `${path} is required array`;
@@ -83,18 +85,22 @@ const validateOneOf = (types, type, uses, path, target, props) => {
         }
     }
 
-    const list = props.list;
+    const list = props.types;
 
     const error = list.reduce((error, typeName, i) => {
-        if (error) {
-            return error;
+        if (! error) {
+            return false;
         }
 
-        return validate(types, typeName, Uses.Required, `${path}(${i})`, target, {});
-    }, undefined);
+        const baseTypeName = types[typeName].$;
+        const baseType = types[baseTypeName];
+        const key = baseType.defined ? baseTypeName : i;
+
+        return validate(types, typeName, Uses.Required, `${path}(${key})`, target, {});
+    }, "never compability");
 
     if (error) {
-        return error;
+        return `${path} must implements one of defined types:\n${error}`;
     }
 };
 
@@ -112,12 +118,62 @@ const validateInteger = (types, type, uses, path, target, props) => {
     }
 };
 
+const validateFloat = (types, type, uses, path, target, props) => {
+    if (target === undefined || target === null) {
+        if (uses === Uses.Required) {
+            return `${path} is required float`;
+        } else if (uses === Uses.Option) {
+            return;
+        }
+    }
+
+    if (! Number.isFinite(target)) {
+        return `${path} must be float`;
+    }
+};
+
+const validateString = (types, type, uses, path, target, props) => {
+    if (target === undefined || target === null) {
+        if (uses === Uses.Required) {
+            return `${path} is required string`;
+        } else if (uses === Uses.Option) {
+            return;
+        }
+    }
+
+    if (! util.isString(target)) {
+        return `${path} must be string`;
+    }
+};
+
+const validateValue = (types, type, uses, path, target, props) => {
+    if (target === undefined || target === null) {
+        if (uses === Uses.Required) {
+            return `${path} is required value`;
+        } else if (uses === Uses.Option) {
+            return;
+        }
+    }
+
+    const valueType = props.type;
+    const value = props.value;
+
+    if (target !== value) {
+        const valueView = valueType === "string" ? `"${value}"` : value;
+
+        return `${path} must be equal to ${valueType} ${valueView}`;
+    }
+};
+
 const defaultTypes = {
     $: { validator: validate },
     integer: { validator: validateInteger },
+    float: { validator: validateFloat },
+    string: {validator: validateString },
     object: { validator: validateObject },
-    array: { validator: validateArray },
+    array_of: { validator: validateArrayOf },
     one_of: { validator: validateOneOf },
+    value: { validator: validateValue },
 };
 
 module.exports = {
--- index.spec.js
+++ index.spec.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { Uses } = require("./index");
 const { defaultTypes } = require("./defaultTypes");
 
-const src = "./test/sample.at.json";
+const src = "../anytype/test/Widget.at.json" || "./test/sample.at.json";
 const schema = JSON.parse(fs.readFileSync(src));
 
 const target = JSON.parse(fs.readFileSync("./test/sample.json"));
@@ -10,6 +10,6 @@ const target = JSON.parse(fs.readFileSync("./test/sample.json"));
 const error = defaultTypes.$.validator({
     ...defaultTypes,
     ...schema.types,
-}, "document", Uses.Required, "document", target, {});
+}, "$", Uses.Required, "$", target, {});
 
 console.log(error);
